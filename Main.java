70f1817dadeed822eb2e1df26e13b60c54fa2a9b
Kuldeep011 <92682190+Kuldeep011@users.noreply.github.com>
2023-05-20 10:39:48 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Main 
+{
+    private static Map<String, Integer> memo = new HashMap<>();
+    public static void main(String[] args)
+    {
+        int row = 5; 
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j <= row - i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) 
+            {
+                int cof = Coefficient(i, j);
+                System.out.print(cof + " ");
+            }
+            System.out.println();
+        }
+    }
+    static int Coefficient(int row, int col) 
+    {
+        if (col == 0 || col == row) 
+        {
+            return 1;
+        }
+
+        String key = row + "," + col;
+        if (memo.containsKey(key)) 
+        {
+            return memo.get(key);
+        }
+
+        int cof = Coefficient(row - 1, col - 1) + Coefficient(row - 1, col);
+        memo.put(key, cof);
+        return cof;
+    }
+}
